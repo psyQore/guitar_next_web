8bb6b53d06b1cd30c7b9729f04590f8b30918b95
Juan Galarce <j6alarce@gmail.com>
2022-07-13 13:23:14 -0400

    Se finaliza los estilos y mediaquery de header

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,17 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className="contenedor">
+        <div className={styles.bar}>
         <Link href="/"> 
           <Image width={400} height={100} src="/img/logo.svg" alt="logo"/>
         </Link>
-        <nav className={styles.navegacion}>
+        <nav className={styles.navegation}>
           <Link href="/">Inicio</Link>
           <Link href="/about">Nosotros</Link>
           <Link href="/blog">Blog</Link>
           <Link href="/store">Store</Link>
         </nav>
+        </div>
       </div>
     </header>
   );
