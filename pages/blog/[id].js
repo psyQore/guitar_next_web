a52bd01a437c354a4cafa4d4426d5cf6b7ac9bc1
Juan Galarce <j6alarce@gmail.com>
2022-07-15 17:21:13 -0400

    Se hace la consulta para cada entrada del blog dinamicamente

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,26 +1,50 @@
-
-
 const EntryBlog = ({ entry }) => {
-
-    console.log(entry);
   return (
     <div>
-        <h1>Desde EntryBlog</h1>
+      <h1>Desde EntryBlog</h1>
     </div>
-  )
+  );
+};
+
+export async function getStaticPaths() {
+  const url = "http://localhost:1337/blogs";
+  const response = await fetch(url);
+  const entries = await response.json();
+
+  const paths = entries.map(entry => ({
+    params: { id: entry.id.toString() },
+  }));
+
+  // console.log(entries);
+  return {
+    paths,
+    fallback: false
+  }
 }
 
-export async function getServerSideProps({ query: {id}}){
+export async function getStaticProps({ params: { id } }) {
+  const url = `http://localhost:1337/blogs/${id}`;
+  const response = await fetch(url);
+  const entry = await response.json();
 
-    const url = `http://localhost:1337/blogs/${id}`;
-    const response = await fetch(url);
-    const entry = await response.json();
-  
-    return {
-        props: {
-            entry
-        }
-    }
+  return {
+    props: {
+      entry,
+    },
+  };
 }
 
-export default EntryBlog
+// export async function getServerSideProps({ query: {id}}){
+
+//     const url = `http://localhost:1337/blogs/${id}`;
+//     const response = await fetch(url);
+//     const entry = await response.json();
+
+//     return {
+//         props: {
+//             entry
+//         }
+//     }
+// }
+
+export default EntryBlog;
