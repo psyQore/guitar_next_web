9a1826711c33802f4905bfb9a429e8e681c606ae
Juan Galarce <j6alarce@gmail.com>
2022-07-14 21:38:34 -0400

    Se crea una bd con strapi y mongodb, se crea una entrada y para usarla en next con serversideprops, se hace la consulta y funciona, se tiene que instalar node 14 para que funciona xd

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({result}) => {
+  console.log(result);
   return (
     <>
       <Layout page="Blog">
@@ -10,4 +11,18 @@ const Blog = () => {
   );
 };
 
+// Forma de consultar una API de Nextjs
+export async function getServerSideProps() {
+  
+  
+  const url = 'http://localhost:1337/blogs'
+  const response = await fetch(url)
+  const result = await response.json()
+
+    return {
+      props: {
+        result
+      }
+    }
+  }
 export default Blog;
