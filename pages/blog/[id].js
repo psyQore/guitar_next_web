05c994358796c1937beeb4c9022e3552d6487d65
Juan Galarce <j6alarce@gmail.com>
2022-07-15 15:25:57 -0400

    Se lee el id se la pagina actual, se hace la llamada a la api para traer los datos y mostrarlos en el navegador

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,13 +1,26 @@
-import { useRouter } from 'next/router'; 
 
 
-const EntryBlog = () => {
-    const router = useRouter();
+const EntryBlog = ({ entry }) => {
 
-    console.log(router.query);
+    console.log(entry);
   return (
-    <div>Desde EntryBlog</div>
+    <div>
+        <h1>Desde EntryBlog</h1>
+    </div>
   )
+}
+
+export async function getServerSideProps({ query: {id}}){
+
+    const url = `http://localhost:1337/blogs/${id}`;
+    const response = await fetch(url);
+    const entry = await response.json();
+  
+    return {
+        props: {
+            entry
+        }
+    }
 }
 
 export default EntryBlog
