dbd2ff0a28c4c084e6db32769456be9861c8bcb6
Juan Galarce <j6alarce@gmail.com>
2022-07-15 08:29:17 -0400

    Se modifica la configuracion de next para mostrar las img de cloidinary

--- components/Entry.js
+++ components/Entry.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 import { formatDate } from "../helpers";
 
 const Entry = ({ entry }) => {
   const { title, resume, image, published_at, id } = entry;
   return (
     <article>
+      <Image
+        layout="responsive"
+        width={800}
+        height={600}
+        src={image.url}
+        alt={`imagen blog ${title}`}
+      />
       <div>
         <h1>{title}</h1>
         <p>{formatDate(published_at)}</p>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images:{
+    domains: ["res.cloudinary.com"]
+  }
 }
 
 module.exports = nextConfig
