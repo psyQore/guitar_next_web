7e32fd57683611a616c0a80d5c3beba7c243e3c7
Juan Galarce <j6alarce@gmail.com>
2022-07-18 15:03:40 -0400

    Se realiza una consulta doble a la api para traer las guitarras y la seccion curso creada en strapi

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,40 @@
 import Layout from "../components/Layout";
-import List from '../components/List';
+import List from "../components/List";
 
-export default function Home({ guitars }) {
+export default function Home({ guitars, course }) {
+  console.log(guitars, course);
   return (
     <>
       <Layout page="Inicio">
         <main className="contenedor">
           <h1 className="heading">Nueestra Coleección</h1>
 
-          <List guitars={guitars}/>
+          <List guitars={guitars} />
         </main>
       </Layout>
     </>
   );
 }
 
+// Consulta a la API doble
 export async function getServerSideProps() {
-  const url = `${process.env.API_URL}/guitars?_sort=price:desc`;
-  const response = await fetch(url);
-  const guitars = await response.json();
+  const urlGuitars = `${process.env.API_URL}/guitars?_sort=price:desc`;
+  const urlCourses = `${process.env.API_URL}/courses`;
+
+  const [resGuitars, resCourse] = await Promise.all([
+    fetch(urlGuitars),
+    fetch(urlCourses),
+  ]);
+
+  const [guitars, course] = await Promise.all([
+    resGuitars.json(),
+    resCourse.json(),
+  ]);
+
   return {
     props: {
       guitars,
+      course,
     },
   };
 }
